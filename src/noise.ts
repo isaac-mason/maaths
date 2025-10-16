f081a3cb76f87b39bc740aae264bfa4778e340d5
“Isaac <isaac@isaacmason.com>
2025-10-17 00:00:55 +1000

    fix: jsdoc

--- src/noise.ts
+++ src/noise.ts
@@ -1,31 +1,26 @@
 import { fade, lerp } from './common';
 
-/*
- * adapted from maath:
- *
- * https://github.com/pmndrs/maath/blob/main/packages/maath/src/random/noise.ts
- *
- * credits from maath:
- *
- * ```
- * Adapated from https://github.com/josephg/noisejs
- * ```
- *
- * ```
- * A speed-improved perlin and simplex noise algorithms for 2D.
- *
- * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
- * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
- * Better rank ordering method by Stefan Gustavson in 2012.
- * Converted to Javascript by Joseph Gentle.
- *
- * Version 2012-03-09
- *
- * This code was placed in the public domain by its original author,
- * Stefan Gustavson. You may use it as you see fit, but
- * attribution is appreciated.
- * ```
- */
+// adapted from maath:
+// https://github.com/pmndrs/maath/blob/main/packages/maath/src/random/noise.ts
+//
+// credits from maath:
+// ```
+// Adapated from https://github.com/josephg/noisejs
+// ```
+// ```
+// A speed-improved perlin and simplex noise algorithms for 2D.
+//
+// Based on example code by Stefan Gustavson (stegu@itn.liu.se).
+// Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
+// Better rank ordering method by Stefan Gustavson in 2012.
+// Converted to Javascript by Joseph Gentle.
+//
+// Version 2012-03-09
+//
+// This code was placed in the public domain by its original author,
+// Stefan Gustavson. You may use it as you see fit, but
+// attribution is appreciated.
+// ```
 
 export type NoiseGenerator2D = (x: number, y: number) => number;
 export type NoiseGenerator3D = (x: number, y: number, z: number) => number;
